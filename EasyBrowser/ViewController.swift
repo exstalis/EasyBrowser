2fd465c916edfa93e448d91dcf9c4d5b30a3b079
exstalis <exstalis@gmail.com>
2016-10-08 18:10:43 +0300

    Initial commit

--- EasyBrowser/ViewController.swift
+++ EasyBrowser/ViewController.swift
@@ -7,17 +7,27 @@
 //
 
 import UIKit
-
-class ViewController: UIViewController {
-
+import WebKit
+class ViewController: UIViewController, WKNavigationDelegate {
+    var webView: WKWebView!
+    
+    override func loadView() {
+        webView = WKWebView()
+        webView.navigationDelegate = self
+        view = webView
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let url = URL(string: "https://www.hackingwithswift.com")!
+        webView.load(URLRequest(url: url))
+        webView.allowsBackForwardNavigationGestures = true
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
 

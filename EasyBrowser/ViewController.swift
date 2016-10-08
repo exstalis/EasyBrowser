cbd62d37229bf1f006d37201b509f2585c61e3dd
exstalis <exstalis@gmail.com>
2016-10-08 19:01:17 +0300

    WKWeb example
    added navigation features and alert view

--- EasyBrowser/ViewController.swift
+++ EasyBrowser/ViewController.swift
@@ -23,11 +23,24 @@ class ViewController: UIViewController, WKNavigationDelegate {
         let url = URL(string: "https://www.hackingwithswift.com")!
         webView.load(URLRequest(url: url))
         webView.allowsBackForwardNavigationGestures = true
+       navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Open", style: .plain, target: self, action: #selector(openTapped))
         
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
+    func openTapped(){
+        let alertController = UIAlertController(title: "Open page...", message: nil, preferredStyle: .actionSheet)
+        alertController.addAction(UIAlertAction(title: "apple.com", style: .default, handler: openPage))
+        alertController.addAction(UIAlertAction(title: "www.hackingwithswift.com", style: .default, handler: openPage))
+        alertController.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        present(alertController, animated: true, completion: nil)
+    }
+    
+    private func openPage(action:UIAlertAction!){
+        let url = URL(string: "https://" + action.title!)!
+        webView.load(URLRequest(url: url))
+    }
+    
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        title = webView.title
     }
 
 
